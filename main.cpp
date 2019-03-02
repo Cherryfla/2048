811056b4b52242e7e67fe0dd55adac55f137cb3b
一只鱼笔 <843479000@qq.com>
2019-03-02 19:21:28 +0800

    Add some features

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<unistd.h>
 #include<ctime>
 #include<iostream>
+#include<cmath>
 using namespace std;
 const int N=4;
 const char GameBox[10][30]={
@@ -16,6 +17,13 @@ const char GameBox[10][30]={
     "+-----+-----+-----+-----+\n",
 };
 int Num[4][4],num[4][4];
+int Color[11]={0,1,1,2,2,3,3,4,4,5,5};
+void Color_init(){
+	for(int i=1;i<8;i++)
+		init_pair(i,i,0);
+	init_pair(8,COLOR_WHITE,COLOR_RED);
+	init_pair(9,0,COLOR_GREEN);
+}
 void GetNewNumber(){	//生成新数字
 	int x=rand()%4,y=rand()%4;
 	while(Num[x][y]!=0){
@@ -81,8 +89,11 @@ int main(){
     noecho();		//可视化输入字符
     cbreak();		//开启字符输入
     srand(time(0));	//初始化随机数
-	curs_set(0);//隐藏光标
-
+	curs_set(0);	//隐藏光标
+	start_color();
+	Color_init();
+	// init_pair(1, COLOR_WHITE, COLOR_RED);
+	// attron(COLOR_PAIR(1));
     keypad(stdscr, TRUE);
     clear();
     int key=0;
@@ -127,11 +138,18 @@ int main(){
 	    				Num[i][j]<<=1,Num[i-1][j]=0;
 	    	MoveDown();
 	    }
-	    int Changed=0;
-	    for(int i=0;i<4;i++)
-	    	for(int j=0;j<4;j++)
+
+	    int Changed=0,win=0,lose=1;
+	    for(int i=0;i<4;i++){
+	    	for(int j=0;j<4;j++){
 	    		if(Num[i][j]!=num[i][j])
 	    			Changed=1;
+	    		if(Num[i][j]==2048)
+	    			win=1;
+	    		if(Num[i][j]==0)
+	    			lose=0;
+	    	}
+	    }
 	    if(Changed)			//如果所有数字都没有移动，就不再生成新数字	
 	    	GetNewNumber();
 	    erase();
@@ -142,11 +160,36 @@ int main(){
 	        for(int j=0;j<4;j++){
 	            if(Num[i][j]==0)
 	            	continue;
-	            move(2*i+1,1+6*j);
-	            printw("%d",Num[i][j]);
+	            // move(2*i+1,1+6*j);
+	            attron(COLOR_PAIR(Color[(int)log2(Num[i][j])]));
+	            mvprintw(2*i+1,1+6*j,"%d",Num[i][j]);
+	        	attroff(COLOR_PAIR(Color[(int)log2(Num[i][j])])); 
 	        }
 	    }
+
+	    if(win){
+	    	attron(COLOR_PAIR(8));
+	    	mvprintw(4,8,"You Win");
+	    	attroff(COLOR_PAIR(8));
+	    }
+	    else if(lose){
+	    	attron(COLOR_PAIR(8));
+	    	mvprintw(4,8,"Game Over");
+	    	attroff(COLOR_PAIR(8));
+	    }
+	    
+	    attron(COLOR_PAIR(9));
+	    mvprintw(10,0,"Press 'r' to reset,Press 'q' to quit");
+	    attroff(COLOR_PAIR(9));
+
 	    key=getch();
+	    if(key=='q')
+	    	break;
+	    if(key=='r'){
+	    	memset(Num,0,sizeof(Num));
+	    	GetNewNumber();
+	    	GetNewNumber();
+	    }
 	}
     endwin();
     exit(EXIT_SUCCESS);
